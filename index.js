14559e877226f01b5785ac0242dcb4a1acbeb285
Michal Budzynski <michal@virtualdesign.pl>
2014-07-18 17:31:56 +0200

    [ANAGRAMS] - :bulb: indentation change

--- index.js
+++ index.js
@@ -2,22 +2,22 @@ var one = 'ccab cdae\'f';
 var two = 'fabecadcc';
 
 var isAnagram = function(first, second) {
-    // remove all non alphanumeric characters
-    // like spaces and aphostrophes
-    first = first.replace(/[^azA-Z0-9]/gi, "");
-    second = second.replace(/[^azA-Z0-9]/gi, "");
+  // remove all non alphanumeric characters
+  // like spaces and aphostrophes
+  first = first.replace(/[^azA-Z0-9]/gi, "");
+  second = second.replace(/[^azA-Z0-9]/gi, "");
 
-    var firstMap = {};
-    var secondMap = {};
+  var firstMap = {};
+  var secondMap = {};
 
-    var createMap = function(char) {
-        if (!this[char]) {
-            this[char] = 0;
-        }
-        this[char]++;
-    }
+  var createMap = function(char) {
+      if (!this[char]) {
+          this[char] = 0;
+      }
+      this[char]++;
+  }
 
-    first.split('').forEach(createMap.bind(firstMap));
-    second.split('').forEach(createMap.bind(secondMap));
-    return Object.equals(firstMap, secondMap);
+  first.split('').forEach(createMap.bind(firstMap));
+  second.split('').forEach(createMap.bind(secondMap));
+  return Object.equals(firstMap, secondMap);
 }
